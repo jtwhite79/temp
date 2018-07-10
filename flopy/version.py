0508a9898ca3c2c17d63b5700f4f82af60a4a881
Jeremy White <jeremyw@vpn-49-2.gns.cri.nz>
2018-07-10 15:51:45 +1200

    added flopy

---
+++ flopy/version.py
@@ -0,0 +1,12 @@
+# flopy version file automatically created using...pre-commit.py
+# created on...June 05, 2018 18:50:12
+
+major = 3
+minor = 2
+micro = 9
+build = 147
+commit = 2818
+
+__version__ = '{:d}.{:d}.{:d}'.format(major, minor, micro)
+__build__ = '{:d}.{:d}.{:d}.{:d}'.format(major, minor, micro, build)
+__git_commit__ = '{:d}'.format(commit)
